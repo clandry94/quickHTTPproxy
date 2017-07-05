92f4860d228bcecc7680cb9318b2c3b93ea9630d
Conor Landry <clandry94@ufl.edu>
2017-07-05 17:10:18 -0400

    Make queue threadsafe

--- src/queue/queue.go
+++ src/queue/queue.go
@@ -1,17 +1,31 @@
 package queue
 
+import (
+	"sync"
+)
+
 type Queue []string
 
+var mutex = &sync.Mutex{}
+
 func (q *Queue) Push(s string) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	*q = append(*q, s)
+	return
 }
 
 func (q *Queue) Pop() (s string) {
+	defer mutex.Unlock()
+	mutex.Lock()
 	s = (*q)[0]
 	*q = (*q)[1:]
 	return
 }
 
 func (q *Queue) Len() int {
-	return len(*q)
+	defer mutex.Unlock()
+	mutex.Lock()
+	len := len(*q)
+	return len
 }
