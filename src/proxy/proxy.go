50edf58444d69621c3422605a283c07dcfb3a859
Conor Landry <clandry94@ufl.edu>
2017-08-03 17:24:39 -0400

    Remove self made queues in favor of channels

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -1,50 +1,47 @@
 package proxy
 
 import (
-	"github.com/clandry94/quickHTTPproxy/src/queue"
 	"github.com/clandry94/quickHTTPproxy/src/spec"
 	"github.com/ivahaev/go-logger"
 	"net"
 )
 
+const MaxConnections = 50
+
 type Handler struct {
-	RankedQueueMap *queue.RankedQueueMap
+	NewConnections chan net.Conn
 	WorkerCount    int
 	Port           string
 }
 
 func New(s *spec.ProxySpec) *Handler {
 	logger.Info("Creating new proxy handler")
-	rqm := queue.NewRankedQueueMap()
-
-	for _, queueConfig := range s.HandlerSpec.QueueConfigs {
-		rankedQueue := queue.NewRankedQueue(queueConfig.Tag, queueConfig.Priority)
-		rqm.Insert(&rankedQueue)
-	}
 
 	return &Handler{
-		RankedQueueMap: rqm,
+		NewConnections: make(chan net.Conn, MaxConnections),
 		WorkerCount:    s.HandlerSpec.WorkerCount,
 		Port:           s.HandlerSpec.Port,
 	}
 }
 
 func (h *Handler) Listen() error {
-	go NewConnWorkerPool(2, h.RankedQueueMap)
 	ln, err := net.Listen("tcp", h.Port)
+
 	if err != nil {
 		logger.Error(err)
 		return err
 	}
+
 	logger.Info("Listening...")
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			logger.Error(err)
 			return err
 		}
-		h.RankedQueueMap.RankedQueueMap["conor"].Push(conn)
-		logger.Info("Pushed conn onto queue")
-		logger.Info("Queue size is now: ", h.RankedQueueMap.RankedQueueMap["conor"].Len())
+
+		h.NewConnections <- conn
+		logger.Info("Pushed conn onto channel")
 	}
 }
